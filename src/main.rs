495bb8b987213bf70702a518b8de3c90b9d667c7
tanque <paoletti.francesco@gmail.com>
2024-08-21 22:17:38 +0200

    (#1) feat(app): add config management for app settings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::fmt::Display;
 
+mod settings;
+use settings::Settings;
+
 #[derive(Debug)]
 struct AppSplashMessage {
     pub title: String,
@@ -112,7 +115,12 @@ impl Display for AppSplashMessage {
 async fn main() {
     let message = AppSplashMessage::from_title("TANQUE WEBSITE - WELCOME");
 
-    println!("{}", message.to_string())
+    println!("{}", message.to_string());
+
+    std::env::set_var("RUST_ENV", "production");
+    let settings = Settings::load().unwrap();
+
+    println!("SETTINGS LOADED:\n{:?}", settings)
 }
 
 #[cfg(test)]
---
+++ src/settings.rs
@@ -0,0 +1,89 @@
+use config::{Config, ConfigError, File};
+use dotenvy::dotenv;
+use serde::{Deserialize, Serialize};
+
+const DEAFULT_CONFIG_FILE: &str = "configs/default";
+const CONFIG_FILE_PREFIX: &str = "configs/";
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
+pub struct Settings {
+    pub env: String,
+    pub log: String,
+    pub host: String,
+    pub port: u16,
+}
+
+impl Settings {
+    pub fn load() -> Result<Self, ConfigError> {
+        dotenv().ok();
+        let env = std::env::var("RUST_ENV").unwrap_or_else(|_| "development".into());
+
+        let settings = Config::builder()
+            .add_source(File::with_name(DEAFULT_CONFIG_FILE))
+            .add_source(File::with_name(&format!("{}{}", CONFIG_FILE_PREFIX, env)))
+            // .set_override("env", env)?
+            .build()?;
+
+        settings.try_deserialize()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serial_test::serial;
+
+    #[tokio::test]
+    #[serial]
+    async fn default_settings() {
+        std::env::set_var("RUST_ENV", "default");
+
+        let default_settings = Settings::load().unwrap();
+
+        assert_eq!(default_settings.env, "development");
+        assert_eq!(default_settings.host, "localhost");
+        assert_eq!(default_settings.log, "warn");
+        assert_eq!(default_settings.port, 5000);
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn development_settings() {
+        std::env::set_var("RUST_ENV", "development");
+
+        let development_settings = Settings::load().unwrap();
+
+        assert_eq!(development_settings.env, "development");
+        assert_eq!(development_settings.host, "0.0.0.0");
+        assert_eq!(development_settings.log, "debug");
+        assert_eq!(development_settings.port, 5000);
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn production_settings() {
+        std::env::set_var("RUST_ENV", "production");
+
+        let production_settings = Settings::load().unwrap();
+
+        println!("production_settings: {:?}", production_settings);
+
+        assert_eq!(production_settings.env, "production");
+        assert_eq!(production_settings.host, "0.0.0.0");
+        assert_eq!(production_settings.log, "error");
+        assert_eq!(production_settings.port, 5000);
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn testing_settings() {
+        std::env::set_var("RUST_ENV", "testing");
+
+        let testing_settings = Settings::load().unwrap();
+
+        assert_eq!(testing_settings.env, "testing");
+        assert_eq!(testing_settings.host, "0.0.0.0");
+        assert_eq!(testing_settings.log, "debug");
+        assert_eq!(testing_settings.port, 5000);
+    }
+}
